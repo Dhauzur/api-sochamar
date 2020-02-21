42e1639e6cf7a60ed062ba5c7d6fa1e2be4fdf3d
Mauro Perez <mmauroperez@gmail.com>
2020-02-21 19:06:14 -0300

    sin start

--- config.js
+++ config.js
@@ -7,8 +7,6 @@ export const JWT_EXPIRATION = process.env.JWT_EXPIRATION;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const URLDB = process.env.URLDB;
 export const NODE_ENV = process.env.NODE_ENV;
-export const PASSWORD_RECOVERY_JWT_EXPIRATION =
-  process.env.PASSWORD_RECOVERY_JWT_EXPIRATION;
 console.log(API_URL);
 console.log(FRONTEND_URL);
 console.log(JWT_ALGORITHM);
