4d117c80acecd43ad274da8fc950296ee2c0a48c
Mauro Perez <mmauroperez@gmail.com>
2020-02-22 19:20:24 -0300

    test config prod 1

--- server/config/config.js
+++ server/config/config.js
@@ -6,24 +6,16 @@ process.env.PORT = process.env.PORT || 3000;
 // ============================
 //  Enviroment
 // ============================
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 // ============================
 //  data base
 // ============================
-let urlDB;
 
-if (process.env.NODE_ENV == 'dev') {
-	urlDB = 'mongodb://localhost:27017/sochamar';
-} else {
-	urlDB =
-		'mongodb+srv://silvita:Perez123@api-sochamar-awvck.mongodb.net/sochamar';
+if (process.env.NODE_ENV == "dev") {
+  urlDB = "mongodb://localhost:27017/sochamar";
+  process.env.URLDB = urlDB;
+  process.env.JWT_SECRET = "sochamar12345";
+  process.env.JWT_ALGORITHM = "HS256";
+  process.env.JWT_EXPIRATION = 6000;
 }
-process.env.URLDB = urlDB;
-
-// mongodb+srv://sochamar:<password>@sochamarmongodb-ubxn7.gcp.mongodb.net/test?retryWrites=true&w=majority
-
-//jwt
-process.env.JWT_SECRET = 'sochamar12345';
-process.env.JWT_ALGORITHM = 'HS256';
-process.env.JWT_EXPIRATION = 6000;
