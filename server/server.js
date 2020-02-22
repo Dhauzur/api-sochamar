3515e8ae965a217891030ce4ea69d0799543d2e6
Mauro Perez <mmauroperez@gmail.com>
2020-02-22 19:32:46 -0300

    log db mongo

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-require('./config/config');
-const express = require('express');
+require("./config/config");
+const express = require("express");
 const app = express();
-const passport = require('passport');
-const passportConfig = require('./config/passport');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const routes = require('./routes/index');
-const path = require('path');
+const passport = require("passport");
+const passportConfig = require("./config/passport");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const routes = require("./routes/index");
+const path = require("path");
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, "uploads")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -20,10 +20,11 @@ app.use(passport.initialize());
 routes(app);
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, err => {
-	if (err) throw err;
-	console.log('Base de datos ONLINE');
+  if (err) throw err;
+  console.log("Base de datos ONLINE");
 });
 
 app.listen(process.env.PORT, () => {
-	console.log('Escuchando puerto: ', process.env.PORT);
+  console.log("MONGO DB:: ", process.env.URLDB);
+  console.log("Escuchando puerto: ", process.env.PORT);
 });
