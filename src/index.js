967a98ad817f89b79c8326dfb57fed286f44df52
Mauro Perez <mmauroperez@gmail.com>
2020-02-23 12:19:00 -0300

    handler dot  confi

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import express from 'express';
-import mongoose from 'mongoose';
-import passport from 'passport';
-import passportConfig from './config/passport';
-import path from 'path';
-import routes from './routes/index';
+require("dotenv").config();
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import passport from "passport";
+import passportConfig from "./config/passport";
+import path from "path";
+import routes from "./routes/index";
 
 const app = express();
 app.use(cors());
@@ -19,21 +20,21 @@ app.use(passport.initialize());
 routes(app);
 
 mongoose.connect(
-	process.env.URLDB,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-	},
-	err => {
-		if (err) throw err;
-		console.info('Data base online');
-	}
+  process.env.URLDB,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  },
+  err => {
+    if (err) throw err;
+    console.info("Data base online");
+  }
 );
 
 app.listen(process.env.PORT || 3000, () => {
-	console.info('MONGO URL');
-	console.info(process.env.URLDB);
-	console.info(`Listen on port ${process.env.PORT}`);
+  console.info("MONGO URL");
+  console.info(process.env.URLDB);
+  console.info(`Listen on port ${process.env.PORT}`);
 });
