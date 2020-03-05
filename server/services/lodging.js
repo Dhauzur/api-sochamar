13f973a74e266b87c294c22d43967ab75e4d5455
Jesus Garcia <fairplaay16@gmail.com>
2020-03-05 16:59:47 -0400

    mov... function mountotal

--- server/services/lodging.js
+++ server/services/lodging.js
@@ -3,104 +3,45 @@ import Place from '../models/place';
 import moment from 'moment';
 import { logError } from '../config/pino';
 
-const getAll = res => {
-	Lodging.find({}).exec((err, lodgings) => {
-		if (err) return res.status(400).json({ ok: false, err });
-		Lodging.countDocuments({}, (err, length) => {
-			res.json({
-				status: true,
-				lodgings,
-				length,
-			});
+const mountTotal = async body => {
+	// set mount total
+	let breakfast = 0,
+		lunch = 0,
+		dinner = 0,
+		lodging = 0;
+	let service = JSON.parse(body.service);
+	const responseDB = await Place.find({ _id: body.place });
+	const prices = responseDB[0].prices;
+	service.map(arr => {
+		arr.filter((item, index) => {
+			if (index === 0) breakfast = breakfast + item;
+			if (index === 1) lunch = lunch + item;
+			if (index === 2) dinner = dinner + item;
+			if (index === 3) lodging = lodging + item;
 		});
 	});
+	const mountTotal =
+		lodging * prices[3] +
+		dinner * prices[2] +
+		lunch * prices[1] +
+		breakfast * prices[0];
+
+	return mountTotal;
 };
 
-//Ojo esto deberia ser updateOne
-const createOne = async (req, res) => {
+const getAll = async res => {
 	try {
-		let { body } = req;
-
-		// set mount total
-		let breakfast = 0,
-			lunch = 0,
-			dinner = 0,
-			lodging = 0;
-		let service = JSON.parse(body.service);
-		const responseDB = await Place.find({ _id: body.place });
-		const prices = responseDB[0].prices;
-		service.map(arr => {
-			arr.filter((item, index) => {
-				if (index === 0) breakfast = breakfast + item;
-				if (index === 1) lunch = lunch + item;
-				if (index === 2) dinner = dinner + item;
-				if (index === 3) lodging = lodging + item;
-			});
-		});
-		const mountTotal =
-			lodging * prices[3] +
-			dinner * prices[2] +
-			lunch * prices[1] +
-			breakfast * prices[0];
-
-		// save in database
-		const lodgingDB = await Lodging.findOneAndUpdate(
-			{ id: body.id },
-			{
-				group: body.group,
-				start: moment(body.start)
-					.hours(16)
-					.format('YYYY-MM-DD'),
-				end: moment(body.end)
-					.hours(12)
-					.format('YYYY-MM-DD'),
-				service: body.service,
-				place: body.place,
-				passengers: body.passengers,
-				mountTotal,
-			},
-			{ upsert: true }
-		);
+		const lodgings = await Lodging.find({});
+		const length = await Lodging.countDocuments({});
 		res.json({
 			status: true,
-			lodging: lodgingDB,
+			lodgings,
+			length,
 		});
 	} catch (error) {
 		logError(error.message);
 		res.status(400).json({ status: false, error: error.message });
 	}
-};
-
-const deleteAll = res => {
-	Lodging.deleteMany({}, function(err, lodging) {
-		if (err) return res.status(400).json({ ok: false, err });
-		res.json({
-			delete: true,
-			deletedCount: lodging.deletedCount,
-		});
-	});
-};
-
-const deleteAllWithPlace = (req, res) => {
-	let place = req.params.place;
-	Lodging.deleteMany({ place }, function(err, lodging) {
-		if (err) return res.status(400).json({ ok: false, err });
-		res.json({
-			delete: true,
-			deletedCount: lodging.deletedCount,
-		});
-	});
-};
-
-const deleteOneWithPlaceId = (req, res) => {
-	let id = req.params.id;
-	Lodging.deleteMany({ id }, function(err, lodging) {
-		if (err) return res.status(400).json({ ok: false, err });
-		res.json({
-			delete: true,
-			deletedCount: lodging.deletedCount,
-		});
-	});
 };
 
 /**
@@ -122,6 +63,68 @@ const getAllForPlace = async (req, res) => {
 			status: false,
 			error: error.message,
 		});
+	}
+};
+
+const createOne = async (req, res) => {
+	try {
+		const { body } = req;
+		const lodgingDB = await Lodging.findOneAndUpdate(
+			{ id: body.id },
+			{
+				group: body.group,
+				start: moment(body.start)
+					.hours(16)
+					.format('YYYY-MM-DD'),
+				end: moment(body.end)
+					.hours(12)
+					.format('YYYY-MM-DD'),
+				service: body.service,
+				place: body.place,
+				passengers: body.passengers,
+				mountTotal: await mountTotal(body),
+			},
+			{ upsert: true }
+		);
+		res.json({
+			status: true,
+			lodging: lodgingDB,
+		});
+	} catch (error) {
+		logError(error.message);
+		res.status(400).json({ status: false, error: error.message });
+	}
+};
+
+const deleteAll = async res => {
+	try {
+		await Lodging.deleteMany({});
+		res.json({ status: true });
+	} catch (error) {
+		logError(error.message);
+		res.status(400).json({ status: false, error: error.message });
+	}
+};
+
+const deleteAllWithPlace = async (req, res) => {
+	try {
+		const { place } = req.params;
+		await Lodging.deleteMany({ place });
+		res.json({ status: true });
+	} catch (error) {
+		logError(error.message);
+		res.status(400).json({ status: false, error: error.message });
+	}
+};
+
+const deleteOneWithPlaceId = async (req, res) => {
+	try {
+		const { id } = req.params;
+		Lodging.deleteMany({ id });
+		res.json({ status: true });
+	} catch (error) {
+		logError(error.message);
+		res.status(400).json({ status: false, error: error.message });
 	}
 };
 
