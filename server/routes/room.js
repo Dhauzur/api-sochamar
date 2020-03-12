c90fa1388f2a7eb92bfc56cf034ebfa23c810221
Jesus Garcia <fairplaay16@gmail.com>
2020-03-12 11:59:07 -0400

    rooms routes

--- server/routes/room.js
+++ server/routes/room.js
@@ -6,22 +6,22 @@ const roomsRouter = Router();
 /* Ojo, aca podemos optimizar los nombres de ruta quitando cosas como /create o /delete/all*/
 /* Los verbos de por si ya estan dando a entender la accion que se realiza sobre esta ruta*/
 roomsRouter.get(
-	'/periods/:placeId',
+	'/rooms/:placeId',
 	passport.authenticate('jwt', { session: false }),
 	roomsController.getAll
 );
 roomsRouter.post(
-	'/periods',
+	'/rooms',
 	passport.authenticate('jwt', { session: false }),
 	roomsController.create
 );
 roomsRouter.delete(
-	'/periods/all',
+	'/rooms/all',
 	passport.authenticate('jwt', { session: false }),
 	roomsController.deleteAll
 );
 roomsRouter.delete(
-	'/periods/one/:id',
+	'/rooms/one/:id',
 	passport.authenticate('jwt', { session: false }),
 	roomsController.deleteOne
 );
