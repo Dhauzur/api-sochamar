1973914707776f55ad629cf2773a3aea4235d623
Jesus Garcia <fairplaay16@gmail.com>
2020-03-13 08:05:39 -0400

    Update schema payments

--- server/schemas/payments.js
+++ server/schemas/payments.js
@@ -9,13 +9,7 @@ const paymentsSchema = {
 		mount: Joi.string().required(),
 	}),
 	update: Joi.object({
-		idPlace: Joi.string().required(),
-		idLodging: Joi.string().required(),
-		startDate: Joi.string(),
-		endDate: Joi.string(),
-		mount: Joi.string(),
 		comments: Joi.string(),
-		voucher: Joi.object(),
 	}),
 };
 
