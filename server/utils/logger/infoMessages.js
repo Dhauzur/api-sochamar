75e4f689ab90b9a7d32db78790bf11754e1f2330
JavierCollipal <jcollipal1212@gmail.com>
2020-04-14 15:57:14 -0400

    comments added in infoMessages functions

--- server/utils/logger/infoMessages.js
+++ server/utils/logger/infoMessages.js
@@ -1,3 +1,9 @@
+//actions: actualizo,elimino,obtuvo, registro
+//pluralization: un,todos los
+//model: el modelo que estamos usando en esa funcion
+//filter: sirve para especificar que se realizo una accion con un filtro o variable adicional
+// ej: test@test.cl obtuvo todos los lodging con placeId
+// con 'variable'
 export const infoMessages = (email, action, pluralization, model, filter) => {
 	if (filter) {
 		return `${email} ${action} ${pluralization} ${model} ${filter}`;
@@ -5,5 +11,5 @@ export const infoMessages = (email, action, pluralization, model, filter) => {
 		return `${email} ${action} ${pluralization} ${model}`;
 	}
 };
-
+//action: acción especifica, ej: test@test.cl actualizo su avatar, test@test.cl recupero su contraseña, test@test.cl solicito un cambio de contraseña
 export const actionInfo = (email, action) => `${email} ${action}`;
